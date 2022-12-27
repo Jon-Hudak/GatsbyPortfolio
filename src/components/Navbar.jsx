92ef15e59647bf884d0c2fc83a25801919c289a2
Jon-Hudak <shigshagor@gmail.com>
2022-12-26 19:02:01 -0500

    fix: Debugging logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,8 @@ import { AnchorLink } from "gatsby-plugin-anchor-links";
 
 function Navbar() {
   const logo = "../images/logo-no-background.svg"
-  const smallLogo = "../images/logo-only.png"
+  const smallLogo = "../images/logo-no-background.svg"
+  // const smallLogo = "../images/logo-only.png"
   const [hamburgerOpen, setHamburgerOpen] = useState(false)
 
   const handleHamburger = () => {
