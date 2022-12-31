7f6be8bbf88b83998db8a8d800520bfe5a7b29eb
Jon-Hudak <shigshagor@gmail.com>
2022-12-30 19:41:32 -0500

    no message

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -35,7 +35,6 @@ function Hero() {
             src={heroPic}
           />
         </div>
-        <div className="px-5 text-right">Based in Pittsburgh, PA</div>
         <div className="bg-black bg-opacity-50 mx-5 my-5 place-self-center">
           <div
             className={`place-content-center flex-col animate-menuOpen rounded-lg origin-top ${
