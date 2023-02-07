d6a8ac3adc4843a4e0c468042252ef43306a1d5a
Jon-Hudak <shigshagor@gmail.com>
2023-02-06 20:04:50 -0500

    fix: ogmeta

---
+++ src/pages/img/ogmeta.jsx
@@ -0,0 +1,11 @@
+import { StaticImage } from 'gatsby-plugin-image'
+import React from 'react'
+
+function ogmeta() {
+  return (
+    <StaticImage src='../../images/ogMeta.jpg' alt='' />
+    
+  )
+}
+
+export default ogmeta
