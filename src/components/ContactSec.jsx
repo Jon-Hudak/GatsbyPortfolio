2e10201ef7d69596ad4582e85645a63a1af9b833
Jon-Hudak <shigshagor@gmail.com>
2023-02-06 21:38:28 -0500

    fix: aria support on forms

--- src/components/ContactSec.jsx
+++ src/components/ContactSec.jsx
@@ -1,4 +1,4 @@
-import { motion} from "framer-motion"
+import { motion } from "framer-motion"
 import React from "react"
 import { useRef } from "react"
 import { useState } from "react"
@@ -15,7 +15,7 @@ export default function ContactSec({ popup }) {
     nameValid: "",
     emailValid: "",
     formErrors: { name: " ", email: " " },
-    formStyle:{ name:"inputField", email:"inputField" },
+    formStyle: { name: "inputField", email: "inputField" },
     formValid: false,
   })
 
@@ -39,29 +39,31 @@ export default function ContactSec({ popup }) {
     setFormState({ ...formState, formValid: formValid })
   }
   const fieldValidation = e => {
-    const field = e.target.name;
-    const value = e.target.value;
-    const errors = formState.formErrors;
-    const styles = formState.formStyle;
-    let nameValid = formState.nameValid;
-    let emailValid = formState.emailValid;
-    let phoneValid = formState.phoneValid;
-    let companyValid = formState.companyValid;
+    const field = e.target.name
+    const value = e.target.value
+    const errors = formState.formErrors
+    const styles = formState.formStyle
+    let nameValid = formState.nameValid
+    let emailValid = formState.emailValid
+    let phoneValid = formState.phoneValid
+    let companyValid = formState.companyValid
 
     switch (field) {
       case "name":
-        nameValid = value.length > 0;
-        errors.name = nameValid ? " " : "Please enter a name";
-        styles.name = nameValid ? "inputField outline-green-500"
-        : "inputField outline-red-600";
+        nameValid = value.length > 0
+        errors.name = nameValid ? " " : "Please enter a name"
+        styles.name = nameValid
+          ? "inputField outline-green-500"
+          : "inputField outline-red-600"
 
         break
       case "email":
-        emailValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
-        emailValid = !!emailValid;
-        errors.email = emailValid ? " " : "Please enter a valid email";
-        styles.email = emailValid ? "inputField outline-green-500"
-        : "inputField outline-red-600"
+        emailValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i)
+        emailValid = !!emailValid
+        errors.email = emailValid ? " " : "Please enter a valid email"
+        styles.email = emailValid
+          ? "inputField outline-green-500"
+          : "inputField outline-red-600"
         break
       case "phone":
         phoneValid = value.match(/^[(]?\d{3}[)]?[-]?\d{3}[-]?\d{4}$/)
@@ -91,16 +93,16 @@ export default function ContactSec({ popup }) {
       className={`px-5 py-3 w-full max-w-5xl rounded-lg place-self-center shadow-xl ${
         popup ? "" : "cont mt-5"
       }`}
-      initial={{ opacity:popup?1:0, y: popup?0:50 }}
+      initial={{ opacity: popup ? 1 : 0, y: popup ? 0 : 50 }}
       whileInView={{ opacity: 1, y: 0 }}
-      viewport={{ once: true, margin:"0px 0px -10% 0px" }}
+      viewport={{ once: true, margin: "0px 0px -10% 0px" }}
       transition={{ duration: 1 }}
     >
-      {!popup && 
+      {!popup && (
         <h2 ref={ref} className="h2 type">
           Contact Me
         </h2>
-      }
+      )}
       <form
         name="Contact"
         className="flex flex-col w-full md:px-10 my-5"
@@ -112,51 +114,53 @@ export default function ContactSec({ popup }) {
         <label className="formLabel">
           Name:
           <input
+            aria-required
+            aria-invalid={formState.nameValid}
             id={popup ? "popUpName" : "name"}
             name="name"
             type="text"
-            className={
-              formState.formStyle.name
-            }
+            className={formState.formStyle.name}
             onBlur={fieldValidation}
             onChange={handleChange}
             value={formState.name}
             placeholder="Name"
           />
-          <p className="text-red-500 text-sm mt-2 h-3">{formState.formErrors.name}</p>
+          <p className="text-red-500 text-sm mt-2 h-3">
+            {formState.formErrors.name}
+          </p>
         </label>
 
         <label className="formLabel">
           Email:
-        
-        <input
-          id="email"
-          name="email"
-          type="email"
-          className={
-            formState.formStyle.email
-          }
-          onBlur={fieldValidation}
-          onChange={handleChange}
-          value={formState.email}
-          placeholder="Email"
-        />
-        <p className="text-red-500 text-sm mt-2 h-3">{formState.formErrors.email}</p>
+          <input
+            aria-required
+            aria-invalid={formState.emailValid}
+            id="email"
+            name="email"
+            type="email"
+            className={formState.formStyle.email}
+            onBlur={fieldValidation}
+            onChange={handleChange}
+            value={formState.email}
+            placeholder="Email"
+          />
+          <p className="text-red-500 text-sm mt-2 h-3">
+            {formState.formErrors.email}
+          </p>
         </label>
-        
+
         <label className="formLabel">
           Message:
-        
-        <textarea
-          id="message"
-          name="message"
-          type="text"
-          className="inputField overflow-y-auto"
-          rows="6"
-          onChange={handleChange}
-          value={formState.message}
-          placeholder="Message"
-        />
+          <textarea
+            id="message"
+            name="message"
+            type="text"
+            className="inputField overflow-y-auto"
+            rows="6"
+            onChange={handleChange}
+            value={formState.message}
+            placeholder="Message"
+          />
         </label>
         <button
           type="submit"
