07991e1f9050962ea2356e4c36f9f1fc71429fd7
Jon-Hudak <shigshagor@gmail.com>
2023-02-26 22:40:20 -0500

    fix: Color fill effect on firefox

--- src/pages/projects/colorfill.jsx
+++ src/pages/projects/colorfill.jsx
@@ -5,35 +5,39 @@ import color from "../../images/tattootest.jpg"
 import { useEffect } from "react"
 
 export default function ColorFill() {
-   
-    
-    function handleClick(){
-    var element = document.getElementById("sketchImg");
-    
-    element.classList.remove('mask-animation');
-    window.requestAnimationFrame(()=> {
-      element.classList.add('mask-animation');
-    });
-  };
-    
-        
-    
-    useEffect(() => {
-      window.requestAnimationFrame(()=> {
-        
-      
-        const element = document.getElementById("sketchImg");
-      element.classList.add("mask-animation");
-    });
-      
-    }, []);
+  function handleClick() {
+    var element = document.getElementById("sketchImg")
+
+    element.classList.remove("mask-animation")
+    setTimeout(() => {
+      window.requestAnimationFrame(() => {
+        element.classList.add("mask-animation")
+      });
+    }, 100);
+  }
+
+  useEffect(() => {
+    window.requestAnimationFrame(() => {
+      const element = document.getElementById("sketchImg")
+      element.classList.add("mask-animation")
+    })
+  }, [])
   return (
     <Layout>
-      <button className="reset-animation mt-10 bg-accent-blue p-5" onClick={handleClick}>Reset</button>
+      <button
+        className="reset-animation mt-10 bg-accent-blue p-5"
+        onClick={handleClick}
+      >
+        Reset
+      </button>
       <div className="mask-container">
-        <img id="sketchImg"className="sketchImg colorFillImg z-10" src={sketch} alt="" />
+        <img
+          id="sketchImg"
+          className="sketchImg colorFillImg mask-animation z-10"
+          src={sketch}
+          alt=""
+        />
         <img className="colorImg colorFillImg" src={color} alt="" />
-        
       </div>
     </Layout>
   )
